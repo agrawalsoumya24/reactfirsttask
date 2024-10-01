139a36b37b877a779a5cb1e4def19b9fb1739c4c
Soumya Agrawal <agrawalsaumya853@gmail.com>
2024-10-01 19:34:27 +0530

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <div id='root' className="bg-black h-full w-full">
+        <div className="nav">
+            <div className="left"><h6>PORTFOLIO.</h6></div>
+            <div className="right">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Works</a>
+                <a href="">Block</a>
+                <a href="">Contact</a>
+            </div>
+        </div>
+        <div className="center">
+            <div className="center-left">
+                <div className="center-left-container">
+                    <h2>my name is</h2>
+                    <h1>SoumyaAgrawal</h1>
+                    <button>I'M A UI DEVELOPER</button>
+                </div>
+            </div>
+            <div className="center-right">
+              
+            </div>
+        </div>
+      
+    </div>
+
+       </div>
+    </>
+  )
+}
+
+export default App
